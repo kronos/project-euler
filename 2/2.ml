aec14240c7ce33cae3b06d8962b5f309e9f7e739
Ivan Samsonov <hronya@gmail.com>
2014-08-23 13:34:44 +0400

    improve 2nd

--- 2/2.ml
+++ 2/2.ml
@@ -1,11 +1,10 @@
 open Core.Std
 
-let fibonacci =
+let fibonacci max =
   let rec fibonacci_gen f1 f2 a =
-    if f1 > 4_000_000 then f1
+    if f1 > max then f1
     else if (f1 mod 2) = 0 then fibonacci_gen f2 (f1+f2) (a + f1)
     else fibonacci_gen f2 (f1+f2) a in
     fibonacci_gen 1 1 0
 
-
-let ()= Printf.printf "%d\n" fibonacci
+let ()= Printf.printf "%d\n" (fibonacci 4_000_000)
