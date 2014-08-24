f98ff3e312d31f8cc213e8b43128bd563d3daf40
Ivan Samsonov <hronya@gmail.com>
2014-08-24 16:50:05 +0400

    Simplify 3rd and 4th

--- 3/3.ml
+++ 3/3.ml
@@ -1,24 +1,25 @@
 open Core.Std
 
-let rec any l f = match l with
-  | []     -> false
-  | hd::tl -> f hd || any tl f
+let is_prime number primes =
+  List.for_all primes ~f:(fun x -> (number mod x) <> 0)
 
-let is_prime number primes = not (any primes (fun x -> (number mod x) = 0))
-
-let rec next_prime primes = match primes with
+let rec next_prime = function
   | []    -> assert false
-  | hd::_ -> let rec search_prime primes current =
-               if is_prime current primes then current
-               else search_prime primes (current+2) in
-               search_prime primes (hd+2)
+  | (hd::_) as primes ->
+      let rec search_prime primes current =
+        if is_prime current primes
+        then current
+        else search_prime primes (current+2)
+      in
+        search_prime primes (hd+2)
 
 let largest_prime_factor n =
   let rec search_largest_prime primes n current = match n with
     | 1 -> current
     | _ when (n mod current) = 0 -> search_largest_prime primes (n/current) current
     | _ -> let new_primes = current::primes in
-             search_largest_prime new_primes n (next_prime new_primes) in
-               search_largest_prime [2] n 3
+             search_largest_prime new_primes n (next_prime new_primes)
+  in
+    search_largest_prime [2] n 3
 
 let () = Printf.printf "%d\n" (largest_prime_factor 600851475143)
--- 4/4.ml
+++ 4/4.ml
@@ -1,24 +1,24 @@
 (* get a nat number as a list of cifers *)
-let rec cifers n = match n with
+let rec cifers = function
   | 0 -> []
-  | _ -> (n mod 10)::(cifers (n/10))
+  | n -> (n mod 10)::(cifers (n/10))
 
 (* check whether list is a palindrome; half the way is good enough here *)
 let palindrome l = List.for_all2 (=) l (List.rev l);;
 
 (* checks whether number factors in two three-cifer numbers *)
-let is_two_three_cifer_factor n =
-  let rec is_ttcf n x = match x with
-    | _ when n/x >= 1000 -> false
-    | _ when n mod x = 0 -> true
-    | _ -> is_ttcf n (x-1)
+let two_three_cifer_factor n =
+  let rec ttcf n = function
+    | x when n/x >= 1000 -> false
+    | x when n mod x = 0 -> true
+    | x -> ttcf n (x-1)
   in
-    is_ttcf n 999
+    ttcf n 999
 ;;
 
 (* largest palindrome below x which factors into numbers of three cifers *)
 let rec lp x =
-    if palindrome (cifers x) && is_two_three_cifer_factor x
+    if palindrome (cifers x) && two_three_cifer_factor x
     then x
     else lp (x-1)
 ;;
