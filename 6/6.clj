f727e7fc145ae1a282cb685d62be3c9f01e92b10
Ivan Samsonov <hronya@gmail.com>
2015-02-07 18:24:34 +0300

    typo in #6

--- 6/6.clj
+++ 6/6.clj
@@ -1,8 +1,8 @@
 (defn sum [coll]
   (apply + coll))
 
-(def task-5
+(def task-6
   (let [coll (range 1 101) sum_coll (sum coll)]
     (- (* sum_coll sum_coll) (sum (map #(* % %) coll)))))
 
-(println task-5)
+(println task-6)
